d8aa87110880ec7ca326610b0bbd5fd577b3e231
Shreedhar Bhat <shridharbhat1998@gmail.com>
2020-07-04 12:08:25 +0530

    Initial Commit

---
+++ test-cypress-main/src/Components/TaskForm.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const TaskForm = props =>
+  <form>
+    <input
+      type='text'
+      className="p-2 rounded border task-input"
+      placeholder="Add something?"/>
+  </form>
+
+  export default TaskForm
